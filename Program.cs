f82f94cc09f2d1e5fa9b98852c8a820e41527c1c
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-09 20:20:47 +0530

    Greeting Message

--- Program.cs
+++ Program.cs
@@ -8,10 +8,27 @@ namespace linecomparison1
     {
         public static void Main(string[] args)
         {
-            quarant.quadrant();
-            
-            
+           //// linecomparison3.
 
+            //Console.WriteLine("To Compare Both Line by CompareTo method");
+            //Console.WriteLine("***************");
+            //Console.WriteLine("Please Enter the first line coordinates");
+            //int x1 = Convert.ToInt32(Console.ReadLine());
+            //int y1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter start and end points of Second line:");
+            //int x2 = Convert.ToInt32(Console.ReadLine());
+            //int y2 = Convert.ToInt32(Console.ReadLine());
+            //linecomparison3.display(x1, y1, x2, y2);
+
+
+           ////linecomparison2.
+
+             //linecomparison2.Check();
+
+
+            ////CartesianSystem.
+            
+            //CartesianSystem.main();
         }
     }
 }
