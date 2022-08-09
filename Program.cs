42b64c778b318399c83a7dd17aea2ccd5eb3a894
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-09 20:16:59 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//using check quadrant
+
+namespace linecomparison1
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            quarant.quadrant();
+            
+            
+
+        }
+    }
+}
